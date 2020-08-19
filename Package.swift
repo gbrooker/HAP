1a5a908afe643773f11ba8e8ec7aec527850bc4c
Bouke Haarsma <bouke@haarsma.eu>
2020-08-19 23:03:57 +0200

    Replace Libsodium with SwiftCrypto
    Thanks to Guy Brooker<guy.brooker@me.com> for providing the correct CryptoKit API usage.

--- Package.swift
+++ Package.swift
@@ -9,24 +9,17 @@ let package = Package(
         .executable(name: "hap-demo", targets: ["hap-demo"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/Bouke/SRP.git", from: "3.1.0"),
-        .package(url: "https://github.com/Bouke/HKDF.git", from: "3.1.0"),
-        .package(url: "https://github.com/IBM-Swift/BlueCryptor.git", from: "1.0.21"),
+        .package(url: "https://github.com/Bouke/SRP.git", .branch("swift-crypto")),
         .package(url: "https://github.com/crossroadlabs/Regex.git", from: "1.1.0"),
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.13.0"),
         .package(url: "https://github.com/apple/swift-log.git", Version("0.0.0") ..< Version("2.0.0")),
+        .package(url: "https://github.com/Bouke/swift-crypto.git", .exact("1.1.0-rc.2-patched")),
     ],
     targets: [
-        .systemLibrary(name: "CLibSodium",
-                       pkgConfig: "libsodium",
-                       providers: [
-                           .brew(["libsodium"]),
-                           .apt(["libsodium-dev"])
-                       ]),
         .target(name: "CQRCode"),
         .target(name: "COperatingSystem"),
         .target(name: "HTTP", dependencies: ["NIO", "NIOHTTP1", "NIOFoundationCompat", "COperatingSystem"]),
-        .target(name: "HAP", dependencies: ["SRP", "Cryptor", "Logging", "HKDF", "Regex", "CQRCode", "HTTP", "CLibSodium"]),
+        .target(name: "HAP", dependencies: ["SRP", "Logging", "Regex", "CQRCode", "HTTP", "Crypto"]),
         .target(name: "hap-demo", dependencies: ["HAP", "Logging"]),
         .testTarget(name: "HAPTests", dependencies: ["HAP"]),
     ]
@@ -34,7 +27,7 @@ let package = Package(
 
 #if os(macOS)
     package.products.append(.executable(name: "hap-update", targets: ["HAPUpdate"]))
-    package.targets.append(.target(name: "HAPUpdate", dependencies: []))
+    package.targets.append(.target(name: "HAPUpdate"))
 #endif
 
 #if os(Linux)
